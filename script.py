b6361b5ec42f812cf52384a1500b6da6bf180061
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-04-25 07:10:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import csv
-
+#creates a results.csv file
 with open('results.csv', mode='w', newline='') as f:
     fwriter = csv.writer(f, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
     fwriter.writerow(['student_id', 'student_name', 'exam_id', 'exam_name', 'marks', 'total_marks', 'percentage'])
